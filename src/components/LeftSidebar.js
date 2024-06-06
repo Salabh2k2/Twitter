759fe0f1bf04ab9651c697e5d75f8484da534142
Salabh Rai <salabh.rai@gmail.com>
2024-06-06 14:49:28 +0530

    added profile section and Login and Signup Pages

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -5,6 +5,7 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUserNinja } from "react-icons/fa";
 import { IoBookmarks } from "react-icons/io5";
 import { LuLogOut } from "react-icons/lu";
+import { Link } from 'react-router-dom';
 const LeftSidebar = () => {
   return (
     <div className='w-[20%]'>
@@ -13,12 +14,12 @@ const LeftSidebar = () => {
           <img className='m-4' width= {"34 px"} src="https://logowik.com/content/uploads/images/twitter-x5265.logowik.com.webp" alt="" />
         </div>
         <div className='my-4'>
-          <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
+          <Link  to="/" className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
             <div>
               <FaHome size = "24px"/>
             </div>
             <h1 className='font-bold text-lg ml-2'>Home</h1> 
-          </div>
+          </Link>
           <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
             <div>
               <FaHashtag size = "24px"/>
@@ -31,12 +32,12 @@ const LeftSidebar = () => {
             </div>
             <h1 className='font-bold text-lg ml-2'>Notification</h1> 
           </div>
-          <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
+          <Link to="/profile" className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
             <div>
               <FaUserNinja size = "24px"/>
             </div>
             <h1 className='font-bold text-lg ml-2'>Profile</h1> 
-          </div>
+          </Link>
           <div className='flex items-center my-2 px-4 py-2 hover:bg-gray-200  cursor-pointer rounded-full '>
             <div>
               <IoBookmarks size = "24px"/>
